1a2c8eba9f56338c498da0349f06c05ffeb7c405
Mason <mason@mason-VirtualBox.(none)>
2013-04-21 21:12:24 -0400

    began yummly search form

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@
  * GET users listing.
  */
 
+var yummlyID = '45928695',
+    yummlyKEY = '32e141669097e9e4be73c86737f3bd3d';
+
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
@@ -11,3 +14,10 @@ exports.login = function(req, res) {
     req.session.url = '/';
 	res.render('login', {title: "Sign In"});
 };
+
+exports.search = function(req, res) {
+	req.session.url = '/search';
+	res.render('search', {
+		title: "Search for new recipes!"
+	});
+}
