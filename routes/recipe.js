5cfc3913fdf73570da5a44c8c8b152271d63b68e
kaustin <kaustin@ubuntu.(none)>
2013-04-30 02:08:57 -0400

    Favorites oddly not functional, push object to array?

--- routes/recipe.js
+++ routes/recipe.js
@@ -69,10 +69,17 @@ exports.addfav = function (req, res){
 				if (add == true){
 					var new_fav = fav_list.push(docs[0])
 						, up_score = docs[0].counter + 1;
-					User.update({username:req.user.username}, {favorites: new_fav}, function(){
+					console.log(new_fav);
+					console.log(docs[0]);
+					console.log(fav_list);
+					User.update({username:req.user.username}, {favorites: fav_list.push(docs[0])}, function(){
 						Recipe.update({'_id': req.body.id}, {counter: up_score}, function() {
-							console.log('Favorite Added');
-							res.redirect('/profile');
+							User.find({username:req.user.username}).exec(function (err, newuse){
+								console.log(newuse);
+								req.user = newuse[0];
+								console.log('Favorite Added');
+								res.redirect('/profile');
+						});
 						});
 					});
 				};
