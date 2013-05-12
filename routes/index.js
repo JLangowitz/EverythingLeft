439e505dbaf9a3d52c7b204dea2fd539e9b78660
Mason <mason@mason-VirtualBox.(none)>
2013-05-12 15:38:34 -0400

    fixed homepage popular/latest

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,19 @@ var Models = require('../models/models')
  * GET home page.
  */
 
+// handle home page information
 exports.index = function(req, res){
-	Recipe.find().sort('counter').exec(function (err, top){
+	Recipe.find().sort({'counter':1}).limit(5).exec(function (err, top){
 		if (err)
 			console.log("Error in Finding Top Recipe");
 
-		Recipe.find().sort('timestamp').exec(function (err, newest){
+		Recipe.find().sort({'timestamp':-1}).limit(5).exec(function (err, newest){
 			if (err)
 				console.log("Error in Finding New Recipes")
+
+			for (i=0; i<newest.length; i++) {
+				console.log(newest[i].timestamp);
+			}
 
 			res.render('index', 
 				{ title: 'Everything Left', 
